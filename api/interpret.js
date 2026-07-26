77012650845ba75a9ae2979569dc1499b6922590
rahulbormon999-star <rahulbormon999@gmail.com>
2026-07-26 10:24:39 +0600

    Increase max_tokens from 1200 to 1500

--- api/interpret.js
+++ api/interpret.js
@@ -171,7 +171,7 @@ export default async function handler(req, res) {
                     model: 'llama-3.3-70b-versatile',
                     messages,
                     temperature: 0.7,
-                    max_tokens: 1200
+                    max_tokens: 1500
                 }),
                 signal: controller.signal
             });
